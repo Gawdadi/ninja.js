8e6787d6b59bc6ed8c864e1d1e52f620cd2424c3
Your Name <you@example.com>
2021-06-03 12:42:37 +0530

    unique values in an array

---
+++ mathematical-problems/uniq-numbers.js
@@ -0,0 +1,20 @@
+// Do not allocate extra space for another array,
+// you must do this by modifying the input array in-place with O(1) extra memory.
+
+var nums = [1,1,1,1,1,2,2,2,2,5,6,7,8,8,8];
+
+// Output = [ 1, 2, 5, 6, 7, 8 ];
+var uniq = function() {
+    var i = 0;
+    while(i < nums.length){
+        const index = nums.indexOf(nums[i]);
+        if(index > -1 && index !== i){
+            nums.splice(index,1);
+        } else {
+             i++;
+        }
+    }
+    return nums;
+};
+
+console.log(uniq());
