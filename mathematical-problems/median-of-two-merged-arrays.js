ddf327a05ad91f3356de7989ca45a2e1cc1a5274
Your Name <you@example.com>
2021-06-04 11:26:01 +0530

    comments added

--- mathematical-problems/median-of-two-merged-arrays.js
+++ mathematical-problems/median-of-two-merged-arrays.js
@@ -5,13 +5,22 @@ var findMedianSortedArrays = function() {
     for(const num of nums2){
         nums1.push(num);
     }
+
+    // Sort merged array.
     nums1.sort((a,b)=>a-b);
     const len = nums1.length
+
+    // If length is one.
     if(len == 1) return nums1[0];
 
+    // Checking for median index.
     const medianIndex = (len - 1)%2;
+
+    // Even length
     if(!medianIndex) return nums1[(len - 1)/2];
         else {
+
+            // Odd length
             const index = Math.floor((len - 1)/2);
             return (nums1[index] + nums1[index + 1])/2;
         }
